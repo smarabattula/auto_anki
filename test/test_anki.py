ecfb10a48237f4474ed91e7bb4bbe10faa96840b
Suneha Bose <sbose2@ncsu.edu>
2021-11-04 00:14:40 -0400

    added test cases for anki

--- test/test_anki.py
+++ test/test_anki.py
@@ -4,7 +4,9 @@ from os import name
 import genanki 
 import unittest
 
-from code.anki import get_deck, get_model
+from genanki.note import Note
+
+from code.anki import add_package, add_question, get_deck, get_model
 
 
 class TestAnki(unittest.TestCase):
@@ -14,3 +16,13 @@ class TestAnki(unittest.TestCase):
     def test_get_deck(self):
         my_deck = get_deck(deck_name='Test Deck Name')
         self.assertEqual(genanki.deck.Deck , type(my_deck))
+
+    def test_add_question(self):
+        model = get_model()
+        note = add_question(question='Test Ques', answer='Ans', curr_model=model)
+        self.assertEqual(genanki.Note, type(note))
+
+    def test_add_package(self)-> None:
+        my_deck = get_deck(deck_name='Test Deck Name')
+        add_package(my_deck, 'output_name')
+        pass
