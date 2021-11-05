3e354016ad2cfefd5dfd957364479bffbd94c79c
Suneha Bose <sbose2@ncsu.edu>
2021-11-04 22:43:08 -0400

    added test case

--- test/test_user_cli.py
+++ test/test_user_cli.py
@@ -3,12 +3,14 @@
 
 import sys
 import unittest
+from unittest.mock import patch
 sys.path.insert(0, 'code')
 import code.user_cli 
 
 class TestUserCLI(unittest.TestCase):
-    
-    def test_user_menu(self) -> None:
+
+    @patch('builtins.input', return_value='1')
+    def test_user_menu(self, mock_input) -> None:
         code.user_cli.user_menu()
 
 
