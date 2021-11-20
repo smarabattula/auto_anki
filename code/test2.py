74c0fce15d5ea0447c0e3a945ffec9778e128af0
SmayanaReddy <smayanap.reddy@gmail.com>
2021-11-19 20:10:34 -0500

    Adding UI componenets
    Added button, label, canvas

--- code/test2.py
+++ code/test2.py
@@ -2,38 +2,8 @@ from tkinter import *
 
 # import filedialog module
 from tkinter import filedialog
-from user_cli import *
-
-def process_(file):
-
-    lect_name = file.split("/")[-1].split(".")[0]
-    raw_data = extract_words(file)
-    raw_data = text_to_groupings(raw_data)
-    keyword_data = wp.extract_noun_chunks(raw_data)
-    keyword_data = wp.merge_slide_with_same_headers(keyword_data)
-
-    keyword_data = wp.duplicate_word_removal(keyword_data)
-    search_query = wp.construct_search_query(
-        keyword_data)
-    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-        # when testing use searchquery[:10 or less].
-        # Still working on better threading to get faster results
-        results = executor.map(get_people_also_ask_links, search_query[:3])
-
-    auto_anki_model = get_model()
-    deck = get_deck(deck_name=lect_name)
-    for result in results:
-        for qapair in result:
-            question = qapair["Question"]
-            answer = qapair["Answer"]
-            qa = add_question(question=f'{question}', answer=f'{answer}', curr_model=auto_anki_model)
-            deck.add_note(qa)
-
-    add_package(deck, lect_name)
-
-
-# Function for opening the
-# file explorer window
+# from user_cli import *
+import random
 def browseFiles():
     # file = filedialog.askopenfilename(initialdir="/",title="Select a File",filetypes=(("Text files","*.txt*"),("all files","*.*")))
     file = filedialog.askopenfile(parent=window, mode='rb', title="Choose a file", filetypes=[("Pdf file", "*.pdf")])
@@ -45,7 +15,9 @@ def browseFiles():
     text_box.tag_add("center", 1.0, "end")
     text_box.grid(column=1, row=4)  
 
-    process_(file)
+    #process_(file)
+    # label_file_explorer.configure(text="File Opened: " + filename)
+
 
 # Create the root window
 window = Tk()
@@ -74,6 +46,7 @@ button_exit = Button(window,
 
 
 button_explore.grid(column=1, row=2)
+
 button_exit.grid(column=1, row=3)
 
 # Let the window wait for any events
