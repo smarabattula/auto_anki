db8727e18ce1880731ec99afbf86383713a744f1
Harikrishnaselvaraj <harikrishnaselva@gmail.com>
2021-11-30 20:43:47 -0500

    Update user_cli.py

--- code/user_cli.py
+++ code/user_cli.py
@@ -51,10 +51,10 @@ def user_menu():
 
 if __name__ == "__main__":
     file, lect_name = user_menu()
-    #if file.endswith(".pdf"):
-    raw_data = extract_words(file)
-    #if file.endswith(".docx"):
-        #raw_data = extract_words_word(file)
+    if file.endswith(".pdf"):
+        raw_data = extract_words(file)
+    if file.endswith(".docx"):
+        raw_data = extract_words_word(file)
 
 
     raw_data = text_to_groupings(raw_data)
