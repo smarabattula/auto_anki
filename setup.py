2ddaec9e38b34ae57387e6c1074c06e4ccb6975f
rachanakondabala <rachana11k@gmail.com>
2021-12-03 17:38:41 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ setup(
     packages=find_packages(),
     description='Upload lecture PDFs and get relevant URLs to helpful websites',
     tests_require=['pytest'],
-    author='CSC510 - Group 21',
+    author='CSC510 - Group 39',
     author_email='lectureaidnscu@gmail.com',
-    url='https://github.com/mtkumar123/CSC510_Project_LectureAid',
+    url='https://github.com/SmayanaReddy/auto_anki',
     python_requires='>=3.7',
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Intended Audience :: Developers",
-        "Topic :: LectureAid",
+        "Topic :: auto_anki",
     ],
     license='MIT',
     install_requires=[
